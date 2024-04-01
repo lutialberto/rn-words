f5ca324664b95f7d879bfad940e247f429cfa99a
Alberto Luti <betomdq.93@gmail.com>
2024-04-01 16:44:32 -0300

    Feat: add textApp component

---
+++ src/components/texts/text/TextApp.tsx
@@ -0,0 +1,25 @@
+import {Text, TextProps} from 'react-native';
+import React from 'react';
+import TextStyles from '~/constants/TextStyles';
+
+/**
+ * @description Application text component
+ * @example Minimal Example
+ * <TextApp>GenericText</TextApp>
+ * @example Full Example
+ * <TextApp
+ *  style={styles.text}
+ * >
+ *  GenericText
+ * </TextApp>
+ * @dependencies TextStyles
+ */
+const TextApp = (props: TextProps) => {
+  return (
+    <Text {...props} style={[TextStyles.textDefault, props.style]}>
+      {props.children}
+    </Text>
+  );
+};
+
+export default TextApp;
