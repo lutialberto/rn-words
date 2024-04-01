f5377bcb40bc81414ef07d4a5c67eec68d9887f3
Alberto Luti <betomdq.93@gmail.com>
2024-04-01 16:20:06 -0300

    Refactor: remove unnecesary code from App component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
-// In App.js in a new project
-
 import * as React from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainNavigator from "~/navigation/MainNavigator";
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
 
 function App() {
   return <MainNavigator />;
