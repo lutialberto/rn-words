c9e5d42a5e30acab25be7a86e244b08d9493083a
Alberto Luti <betomdq.93@gmail.com>
2024-04-01 15:34:23 -0300

    Testing changes

---
+++ src/Asd.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Asd = () => {
+   return (
+      <View>
+         <Text>Asd</Text>
+      </View>
+   );
+};
+
+export default Asd;
+
+const styles = StyleSheet.create({});
