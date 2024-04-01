d43604ecc0b13af0a174069678ae6c3d03d03a5c
Alberto Luti <betomdq.93@gmail.com>
2024-04-01 16:32:01 -0300

    Feat: setup colors file

---
+++ src/constants/Colors.ts
@@ -0,0 +1,43 @@
+const red = '#F5333F';
+const gray = '#969696';
+const blue = '#0186FF';
+const orange = '#FF881A';
+const green = '#0a4a1b';
+const yellow = '#FFCD00';
+const white = '#FFFFFF';
+const black = '#222222';
+const ultraBlack = '#010101';
+
+const NONE_THEME_PROPS = {
+  red,
+  gray,
+  blue,
+  orange,
+  green,
+  yellow,
+  white,
+  black,
+  ultraBlack,
+};
+
+export default {
+  generic: {
+    red,
+    gray,
+    blue,
+    orange,
+    green,
+    yellow,
+    white,
+    black,
+    ultraBlack,
+  },
+  custom: {
+    primary: blue,
+    fontColor: black,
+    backgroundColor: white,
+    cardColor: '#f2f1f0',
+    disabledColor: gray,
+    lowOpacityColor: black,
+  },
+};
