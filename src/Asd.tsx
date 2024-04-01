071d53403674522fe78e73b536f93b4a26aed008
Alberto Luti <betomdq.93@gmail.com>
2024-04-01 15:38:39 -0300

    testing more changes

--- src/Asd.tsx
+++ src/Asd.tsx
@@ -2,11 +2,11 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const Asd = () => {
-   return (
-      <View>
-         <Text>Asd</Text>
-      </View>
-   );
+    return (
+        <View>
+            <Text>Asd3</Text>
+        </View>
+    );
 };
 
 export default Asd;
